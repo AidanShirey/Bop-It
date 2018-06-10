8596f89c8b4d872c218d2c154c16d23251e354f9
AidanShirey <aidan.shirey@gmail.com>
2018-06-09 19:09:29 -0700

    Add files via upload

--- Bop It.cpp
+++ Bop It.cpp
@@ -7,11 +7,18 @@
 #include <string>
 #include "time.h"
 #include <iostream>
+#include <windows.h>
 
 using namespace std;
 
 int main()
 {
+	const WORD colors[] =
+	{
+		0xF, 0xC, 0xE, 0xB, 0xA
+	};
+	HANDLE hstdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	WORD index = 0;
 	string commands[5] = { "Bop it", "Spin it", "Twist it", "Pull it", "Switch it" };
 	srand(time(NULL));
 	ISpVoice * pVoice = NULL;
@@ -24,19 +31,43 @@ int main()
 		{
 			int play = rand() % 5;
 			int playswitch = play + 1;
-			cout << commands[play] << endl;
 			switch (playswitch)
 			{
-			case 1: hr = pVoice->Speak(L"Bop it", 0, NULL);
+			case 1: {
+				index = 0;
+				SetConsoleTextAttribute(hstdout, colors[index]);
+				cout << commands[play] << endl;
+				hr = pVoice->Speak(L"Bop it", 0, NULL);
 				break;
-			case 2: hr = pVoice->Speak(L"Spin it", 0, NULL);
+			}
+			case 2: {
+				index = 1;
+				SetConsoleTextAttribute(hstdout, colors[index]);
+				cout << commands[play] << endl;
+				hr = pVoice->Speak(L"Spin it", 0, NULL);
 				break;
-			case 3: hr = pVoice->Speak(L"Twist it", 0, NULL);
-				break;
-			case 4: hr = pVoice->Speak(L"Pull it", 0, NULL);
-				break;
-			case 5: hr = pVoice->Speak(L"Switch it", 0, NULL);
-				break;
+			}
+			case 3: {
+				index = 2;
+				SetConsoleTextAttribute(hstdout, colors[index]);
+				cout << commands[play] << endl;
+				hr = pVoice->Speak(L"Twist it", 0, NULL);
+				break; 
+			}
+			case 4: {
+				index = 3;
+				SetConsoleTextAttribute(hstdout, colors[index]);
+				cout << commands[play] << endl;
+				hr = pVoice->Speak(L"Pull it", 0, NULL);
+				break; 
+			}
+			case 5: {
+				index = 4;
+				SetConsoleTextAttribute(hstdout, colors[index]);
+				cout << commands[play] << endl;
+				hr = pVoice->Speak(L"Switch it", 0, NULL);
+				break; 
+			}
 			}
 		}
 		
